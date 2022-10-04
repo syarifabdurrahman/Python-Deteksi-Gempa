c238a8c763b7e6a79ada240be9db7e7748b423a8
unknown <syarifabdr22@gmail.com>
2022-10-04 16:15:16 +0700

    Menggunakan beutifulsoup untuk mengolah data dari web

--- gempa_terkini/__init__.py
+++ gempa_terkini/__init__.py
@@ -7,19 +7,68 @@
     dirasakan: Dirasakan (Skala MMI): II Enggano
 """
 
+from turtle import title
+import requests
+import bs4 
+
 
 def ekstraksi_data():
-    hasil = dict ()
-    hasil["tanggal"] = "03 Oktober 2022"
-    hasil["waktu"] = "19:11:59 WIB"
-    hasil["magnitudo"] = "4.1"
-    hasil["kedalaman"] = "2 km"
-    hasil["lokasi"] = {"ls": 5.35,"bt" : 102.57 }
-    hasil["pusat"] = "Pusat gempa berada di laut 33 km tenggara Enggano"
-    hasil["dirasakan"] = "Dirasakan (Skala MMI): II Enggano"
-    return hasil
+    try:
+        content=requests.get('https://www.bmkg.go.id')
+    except Exception:
+        print(Exception)
+        return None
+
+    if content.status_code==200:
+        soup = bs4.BeautifulSoup(content.text,'html.parser')
+
+        result= soup.find('span',{'class':'waktu'})
+        result= result.text.split(',')
+        tanggal= result[0]
+        waktu= result[1]
+    
+        result = soup.find('div',{'class':'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        result =result.findChildren('li')
+        i=0
+        magnitudo=None
+        kedalaman=None
+        ls=None
+        bt=None
+        pusat=None
+        dirasakan=None
+
+        for rest in result:
+            if i == 1:
+                magnitudo = rest.text
+            elif i == 2:
+                kedalaman = rest.text
+            elif i == 3:
+                koordinat = rest.text.split(' - ')
+                ls = koordinat[0]
+                bt = koordinat[1]
+            elif i == 4:
+                pusat = rest.text
+            elif i == 5:
+                dirasakan = rest.text
+            i+=1
+
+        hasil = dict ()
+        hasil["tanggal"] = tanggal
+        hasil["waktu"] = waktu
+        hasil["magnitudo"] = magnitudo
+        hasil["kedalaman"] = kedalaman
+        hasil["lokasi"] = {"ls": ls,"bt" : bt }
+        hasil["pusat"] = pusat
+        hasil["dirasakan"] = dirasakan
+        return hasil
+    else:
+        return None
 
 def tampilkan_data(result):
+    if result is None:
+        print("Tidak menemukan data")
+        return
+
     print("Gempa terakhir berdasarkan BMKG")
     print(f"Tanggal:  {result['tanggal']}")
     print(f"Waktu:  {result['waktu']}")
@@ -29,3 +78,5 @@ def tampilkan_data(result):
     print(f"pusat:  {result['pusat']}")
     print(f"dirasakan:  {result['dirasakan']}")
 
+if __name__ == '__main__':
+    print("haio")
